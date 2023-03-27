21b3ba718ac8e21b4bf403b8b41e331a30ad02de
Jan Shah <ShahJ@ucenmanchester.ac.uk>
2023-03-27 16:33:52 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import json
+from flask import Flask, render_template, redirect, request
+
+app = Flask("main")
+
+income = []
+
+
+@app.route('/')
+def home():
+    total_sales = 0
+    for sale in income:
+        total_sales += float(sale['price'])
+    return render_template('index.html', name="Sales records", sales=income, total=round(total_sales, 2))
+
+@app.route('/add_sale', methods=['POST'])
+def add_income():
+    income.append(
+        {
+            'product': request.form['product'],
+            'price': request.form['price']
+        }
+    )
+    return redirect('/')
+
+def save_sales():
+    with open('sales.txt','w') as file:
+        file.write(json.dumps(income))
+        
+def load_sales():
+    global income
+    with open('sales.txt','r') as file:
+        income = json.loads("".join(file.readlines()))
+    
+
+
+app.run()
